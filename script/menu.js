a67e038e2a6b53e761d292835fab903eade92a93
Reynaldy saputra <renalfrontend@gmail.com>
2020-06-24 17:05:39 +0700

    Edit gambar menjadi file size kecil

---
+++ script/menu.js
@@ -0,0 +1,203 @@
+const Allmenu = [
+     {
+          nama : 'cheeseburger',
+          harga : '50.000',
+          path : '../img/menu/cheeseburger.png',
+     },
+     {
+          nama : 'Chiken strips',
+          harga : '35.000',
+          path : '../img/menu/chicken strips.png'
+     }, 
+     {
+          nama : 'Coca cola',
+          harga : '18.636',
+          path : '../img/menu/Coca cola.png'
+     },
+     {
+          nama : 'croissan wich with egg and cheese',
+          harga : '30.000',
+          path :'../img/menu/croissan wich with egg and cheese.png',
+     },
+     {
+          nama : 'croissan wich with sausage and cheese',
+          harga : '25.000',
+          path : '../img/menu/croissan wich with sausage and cheese.png',
+     },
+     {
+          nama : 'croissan wich with sausage,cheese,egg',
+          harga : '36.909',
+          path : '../img/menu/croissan wich with sausage,cheese,egg.png',
+     },
+     {
+          nama : 'double cheeseburgere',
+          harga : '35.000',
+          path : '../img/menu/double cheeseburgere.png',
+     },
+     {
+          nama : 'double whopper with cheese',
+          harga : '66.364',
+          path : '../img/menu/double whopper with cheese.png',
+     },
+     {
+          nama : 'double whopper,no cheese',
+          harga : '61.364',
+          path : '../img/menu/double whopper,no cheese.png',
+     },
+     {
+          nama : 'french fries',
+          harga : '25.000',
+          path : '../img/menu/french fries.png',
+     },
+     {
+          nama : 'french toast stick',
+          harga : '22.000',
+          path : '../img/menu/french toast stick.png',
+     },
+     {
+          nama : 'Hamburger',
+          harga : '22.000',
+          path : '../img/menu/Hamburger.png',
+     },
+     {
+          nama : 'hash brown rounds',
+          harga : '33.000',
+          path : '../img/menu/hash brown rounds.png',
+     },
+     {
+          nama : 'onion rings',
+          harga : '22.000',
+          path : '../img/menu/onion rings.png',
+     },
+     {
+          nama : 'original chicken sandwich',
+          harga : '20.000',
+          path : '../img/menu/original chicken sandwich.png',
+     },
+     {
+          nama : 'premium fish sandwich',
+          harga : '33.309',
+          path : '../img/menu/premium fish sandwich.png',
+     },
+     {
+          nama : 'vanilla shake',
+          harga : '',
+          path : '../img/menu/vanilla shake.png',
+     },
+     {
+          nama : 'whopper with cheese',
+          harga : '54.090',
+          path : '../img/menu/whopper with cheese.png',
+     },
+     {
+          nama : 'whopper,no cheese',
+          harga : '49.090',
+          path : '../img/menu/whopper,no cheese.png',
+     }
+]
+
+const burger = [
+     { 
+          nama : 'cheeseburger',
+          harga : '50.000',
+          path : '../img/menu/cheeseburger.png',
+     },
+     {
+          nama : 'croissan wich with egg and cheese',
+          harga : '30.000',
+          path : '../croissan wich with egg and cheese.png',
+     },
+     {
+          nama : 'croissan wich with sausage and cheese',
+          harga : '25.000',
+          path : '../img/menu/croissan wich with sausage and cheese.png',
+     },
+     {
+          nama : 'croissan wich with sausage,cheese,egg',
+          harga : '36.909',
+          path : '../img/menu/croissan wich with sausage,cheese,egg.png',
+     },
+     {
+          nama : 'double cheeseburgere',
+          harga : '35.000',
+          path : '../img/menu/double cheeseburgere',
+     },
+     {
+          nama : 'double whopper with cheese',
+          harga : '66.364',
+          path : '../img/menu/double whopper with cheese.png',
+     },
+     {
+          nama : 'double whopper,no cheese',
+          harga : '61.364',
+          path : '../img/menu/double whopper,no cheese.png',
+     },
+     {
+          nama : 'Hamburger',
+          harga : '22.000',
+          path : '../img/menu/Hamburger.png',
+     },
+     {
+          nama : 'original chicken sandwich',
+          harga : '20.000',
+          path : '../img/menu/original chicken sandwich.png',
+     },
+     {
+          nama : 'premium fish sandwich',
+          harga : '33.309',
+          path : '../img/menu/premium fish sandwich.png',
+     },
+     {
+          nama : 'whopper with cheese',
+          harga : '54.090',
+          path : '../img/menu/whopper with cheese.png',
+     },
+     {
+          nama : 'whopper,no cheese',
+          harga : '49.090',
+          path : '../img/menu/whopper,no cheese.png',
+     }
+]
+
+const chicken = [
+     {
+          nama : 'chicken strips',
+          harga : '35.000',
+          path : '../img/menu/chicken strips.png',
+     }
+]
+
+const  fries = [
+     {
+          nama : 'french fries',
+          harga : '25.000',
+          path : '../img/menu/french fries.png',
+     },
+     {
+          nama : 'french toast stick',
+          harga : '22.000',
+          path : '../img/menu/french toast stick.png',
+     },
+     {
+          nama : 'onion rings',
+          harga : '22.000',
+          path : '../img/menu/onion rings.png',
+     },
+     {
+          nama : 'hash brown rounds',
+          harga : '33.000',
+          path : '../img/menu/hash brown rounds.png',
+     }
+]
+const drink = [
+     {
+          nama : 'Coca cola',
+          harga : '18.636',
+          path : '../img/menu/Coca cola.png',
+     },
+     {
+          nama : 'vanilla shake',
+          harga : '',
+          path : '../img/menu/vanilla shake.png',
+     }
+]
