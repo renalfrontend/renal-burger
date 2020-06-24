f8bdebffe14974f00da6af0d59baa71b3d6ecd31
Reynaldy saputra <renalfrontend@gmail.com>
2020-06-24 17:15:57 +0700

    Memperbaruhi fitur img peringatan untuk device mobile

--- script/script.js
+++ script/script.js
@@ -2,3 +2,19 @@ const button = document.querySelector('.menu-toggle');
 const element = document.querySelector('nav ul');
 
 button.addEventListener('click', () => element.classList.toggle('toggle-menu'));
+
+const buttonForm = document.querySelector('#container4 button');
+const smartphoneDevice = window.matchMedia('(max-width : 700px)');
+
+buttonForm.addEventListener('click', function(e) {
+     e.preventDefault();
+
+     Swal.fire({
+          title: 'Maaf',
+          text: 'Restaurant kami telah tutup, kami hanya menerima pesanan order. Terima kasih.',
+          imageUrl: (smartphoneDevice.matches) ? 'img/real/peringatan3.jpg' : 'img/real/peringatan2.jpg',
+          imageWidth: 350,
+          imageHeight: 180,
+          confirmButtonColor: "#FDC74D"
+        })
+})
