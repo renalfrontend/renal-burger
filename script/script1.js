99461f6fc19f65e0723f5a38b85b2d2b35b410ea
Reynaldy saputra <renalfrontend@gmail.com>
2020-06-25 02:01:09 +0700

    Menambahkan 70% project halaman menu

--- script/script1.js
+++ script/script1.js
@@ -4,7 +4,6 @@ const element = document.querySelector('nav ul');
 button.addEventListener('click', () => element.classList.toggle('toggle-menu'));
 
 const buttonForm = document.querySelector('#container4 button');
-const smartphoneDevice = window.matchMedia('(max-width : 700px)');
 
 buttonForm.addEventListener('click', function(e) {
      e.preventDefault();
@@ -12,7 +11,7 @@ buttonForm.addEventListener('click', function(e) {
      Swal.fire({
           title: 'Maaf',
           text: 'Restaurant kami telah tutup, kami hanya menerima pesanan order. Terima kasih.',
-          imageUrl: (smartphoneDevice.matches) ? 'img/real/peringatan3.jpg' : 'img/real/peringatan2.jpg',
+          imageUrl: 'img/real/peringatan2.jpg',
           imageWidth: 350,
           imageHeight: 180,
           confirmButtonColor: "#FDC74D"
